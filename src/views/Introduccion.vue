bddc69ce90969b602daea78fb2d9d40785a3fe42
flizcanor <flizcanor@icloud.com>
2024-12-24 07:06:33 -0500

    se ajusta base

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -2,42 +2,5 @@
 .curso-main-container.introduccion
   BannerInterno(subTitulo="Introducción")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .row.mb-5
-      .col-lg-7
-        p(data-aos="fade-right") En esta unidad se profundiza sobre la importancia de la sostenibilidad en el desarrollo económico, teniendo en cuenta la necesidad de ejecutarla como una constante y un eje transversal y, pese a que aún representa uno de los mayores desafíos éticos y prácticos de nuestra era, nos centramos en reconocer cómo los principios bioéticos pueden orientar decisiones y estrategias hacia un desarrollo económico que respete, tanto el medio ambiente como los derechos humanos. El estudio de esta unidad es complemento de las demás unidades dentro del curso "Bioética, globalización y economía", dado que proporciona una comprensión ética de los efectos que las actividades económicas tienen sobre la sociedad y el planeta, integrando principios íntegros en la toma de decisiones, en entornos globalizados y sostenibles.    
-        p(data-aos="fade-right") Al finalizar esta unidad, se espera que los estudiantes aprueben el resultado de aprendizaje abordado, encontrándose en la capacidad de proponer estrategias y soluciones éticas, aplicables a la sostenibilidad y el desarrollo económico en un contexto global, integrando principios bioéticos. 
-      .col-lg-5
-        figure
-          img(src="@/assets/curso/intro/1.png", data-aos="zoom-in").mt-4.mt-lg-0
 
-    .row.mb-4
-      .col-lg-5
-        figure
-          img(src="@/assets/curso/intro/2.png", data-aos="zoom-in").mt-4.mt-lg-0
-      .col-lg-7
-        p(data-aos="fade-right") Esta unidad ha sido estructurada en tres temas:
-        ol.lista-ol--cuadro(data-aos="fade-up")
-          li 
-            .lista-ol--cuadro__vineta
-              span 1
-            | Aborda la aplicación de la bioética al desarrollo económico sostenible, examinando cómo los principios bioéticos pueden orientar prácticas económicas que respeten el medio ambiente. 
-          li 
-            .lista-ol--cuadro__vineta
-              span 2
-            | Explora el rol de las políticas globales en la protección de los derechos humanos y los recursos naturales, analizando el impacto de las normativas internacionales en la sostenibilidad y el bienestar social. 
-          li 
-            .lista-ol--cuadro__vineta
-              span 3
-            | Se centra en la ética de la innovación y el desarrollo tecnológico, resaltando la importancia de un desarrollo tecnológico responsable, que favorezca la sostenibilidad.
-
-    .row.mb-5
-      .col-lg-7
-
-        .bg-color-1.p-4(data-aos="fade-left").mb-4
-          p(data-aos="fade-left").mb-0 Es el complemento de las anteriores unidades, que pretende ser aplicado en la formación profesional y en la vida diaria, en un contexto donde la crisis climática y los problemas de justicia social, se intensifican.  
-
-        p(data-aos="fade-left") Durante el desarrollo de la unidad, se espera que los estudiantes se integren a cada una de las actividades propuestas, buscando fortalecer cada una de las temáticas estudiadas. Esto no solo demostrará su compromiso, sino que, a su vez, retroalimentará su análisis crítico y la aplicación de los conceptos aprendidos. Es a través del involucramiento constante y la actitud ética, donde se logrará alcanzar una comprensión profunda de los desafíos de sostenibilidad y bioética, en un contexto económico global.
-      .col-lg-5
-        figure
-          img(src="@/assets/curso/intro/3.png", data-aos="zoom-in")
 </template>
